2cf7c9b4473e311e0598e6b5814f856f683bda77
ghosalya <gede_ghosalya@mymail.sutd.edu.sg>
2018-03-22 06:03:44 +0000

    ecb.py working on Tux.ppm

--- lab6/ecb.py
+++ lab6/ecb.py
@@ -2,14 +2,39 @@
 # ECB wrapper skeleton file for 50.020 Security
 # Oka, SUTD, 2014
 from present import *
-import argparse
+import argparse, sys
 
 nokeybits=80
 blocksize=64
 
 
 def ecb(infile,outfile,key,mode):
-    pass
+    """
+    Working Version
+    """
+    if mode.lower()[0] not in 'ed':
+        print("Unrecognized cipher mode [Use `ENC`/`DEC`")
+        return
+
+    with open(infile, 'rb') as inp_file, open(outfile, 'wb') as out_file:
+        while True:
+            chunk = inp_file.read(blocksize//8)#.encode('utf-8')
+            if chunk:
+                chunk_int = int.from_bytes(chunk, byteorder=sys.byteorder)
+                if mode.lower()[0] == 'e':
+                    # encryption
+                    cipher = present(chunk_int, key)
+                elif mode.lower()[0] == 'd':
+                    # decryption
+                    cipher =present_inv(chunk_int, key)
+                else:
+                    print("Unrecognized cipher mode [Use `ENC`/`DEC`]")
+                cipherbytes = cipher.to_bytes(blocksize//8, byteorder=sys.byteorder, signed=False)
+                # cipherstring = cipherbytes.decode('utf-8')
+                out_file.write(cipherbytes)
+            else:
+                break
+
 
 if __name__=="__main__":
     parser=argparse.ArgumentParser(description='Block cipher using ECB mode.')
@@ -22,6 +47,11 @@ if __name__=="__main__":
     infile=args.infile
     outfile=args.outfile
     keyfile=args.keyfile
+    mode=args.mode
 
+    with open(keyfile, 'rb') as keyf:
+        keys = keyf.read(nokeybits//8)
+        key = int.from_bytes(keys, byteorder=sys.byteorder)
 
+    ecb(infile, outfile, key, mode)
 
